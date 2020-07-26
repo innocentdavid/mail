a53e6ae1094b6afc98e591027f72f27f3c9d87e0
innocentdavid <paulinnocent04@gmail.com>
2020-07-26 19:50:48 +0100

    update

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -46,19 +46,18 @@ function cform(r, s, b) {
       body: b
     })
   })
-    .then(response => response.json())
-    .then(result => {
-      // Print result
-      // console.log(result);
-      if (result.message) {
-        alert(result.message);
-        window.location = '';
-      }
-      else {
-        alert(result.error);
-        window.location = '';
-      }
-    });
+  .then(response => response.json())
+  .then(result => {
+    // Print result
+    // console.log(result);
+    if (result.message) {
+      alert(result.message);
+      window.location = '';
+    } else {
+      alert(result.error);
+      window.location = '';
+    }
+  });
 }
 
 function load_mailbox(mailbox) {
@@ -69,43 +68,55 @@ function load_mailbox(mailbox) {
 
   // fetch emails
   fetch(`/emails/${mailbox}`)
-    .then(response => response.json())
-    .then(emails => {
-      if (emails != '') {
+  .then(response => response.json())
+  .then(emails => {
+    if (emails != '') {
 
-        var result = '';
-        result += `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3><br />`;
+      var result = '';
+      result += `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3><br />`;
 
-        emails.forEach(email => {
-          let str = email.body;
-          body = str.slice(0, 100) + ' .......';
+      emails.forEach(email => {
+        let str = email.body;
+        body = str.slice(0, 100) + ' .......';
 
-          result += `<div class="row mb-4"> <div class="col-9"> <a onclick="load_mail(${email.id})" href="#" id="singleEmail"> <h4>${email.sender}</h4> <div><b>SUBJECT:</b> <i>${email.subject}</i></div> <div><b>MESSAGE:</b> ${body}</div> </div></a>`
-          result += `<div class="col-3"> <div style="margin:1rem;">${email.timestamp}</div> <div> <center> <span onclick="alert('reply')" class="btn btn-success">reply</span> <span onclick="alert('archieve')" class="btn btn-primary">achieve</span> </center> </div> </div> </div>`
-        });
+        result += `<div class="row mb-4"> <div class="col-9" style="word-wrap:break-word;"> <a onclick="load_mail(${email.id})" href="#" id="singleEmail"> <h4>${email.sender}</h4> <div><b>SUBJECT:</b> <i>${email.subject}</i></div> <div><b>MESSAGE:</b> ${body}</div> </div></a>`
+        result += `<div class="col-3"> <div style="margin:1rem;">${email.timestamp}</div> <div> <center> <span onclick="alert('reply')" class="btn-sm btn-success" mailId="${email.id}">reply</span> <span onclick="alert('archieve')" class="btn-sm btn-primary" mailId="${email.id}">achieve</span> </center> </div> </div> </div>`
+      });
 
-        document.querySelector('#emails-view').innerHTML = result;
+      document.querySelector('#emails-view').innerHTML = result;
 
-      } else {
-        var result = '';
-        result += `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3><br />`;
-        result += '<ul><li>No emails</li></ul>'
+    } else {
+      var result = '';
+      result += `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3><br />`;
+      result += '<ul><li>No emails</li></ul>'
 
-        document.querySelector('#emails-view').innerHTML = result;
+      document.querySelector('#emails-view').innerHTML = result;
 
-      }
-    });
+    }
+  });
 }
 
 function load_mail(id) {
 
   // fetch email
   fetch(`/emails/${id}`)
-    .then(response => response.json())
-    .then(emails => {
-      console.log(emails);
-      // emails.forEach(email => {
-      //   console.log(email.sender);
-      // });
-    });
+  .then(response => response.json())
+  .then(email => {
+    // console.log(email);
+
+    result = `<div class="row mb-4"> <div style="border:1px solid; word-wrap:break-word;" class="col-12">`;
+
+    result += `<div><b>From:</b> ${email.sender}</div>`;
+    result += `<div class=""><b>To:</b> ${email.recipients}</div>`;
+    result += `<div class=""><b>Subject:</b> ${email.subject}</div>`;
+    result += `<div>${email.timestamp}</div>`;
+    result += `<div class=""><span class="btn-sm btn-primary">reply</span> <span class="btn-sm btn-success ml-auto">archieve</span></div><hr />`;
+    result += `<div class="" style="">${email.body}</div>`;
+    result += ``;
+
+
+    result += '</div> </div>';
+
+    document.querySelector('#emails-view').innerHTML = result;
+  });
 }
